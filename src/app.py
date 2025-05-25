13b045b93fbf65f17d60352b5649e6905f372437
h0bnobs <mb8ld0ck@gmail.com>
2025-05-25 18:48:35 +0100

    fix: change the host setting to listen on all interfaces instead of localhost only

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from query_ai import get_installed_models, get_ai_response
 
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
+
 @app.route('/', methods=['GET'])
 def index() -> str:
     """
@@ -11,6 +12,8 @@ def index() -> str:
     :return: The render template of the homepage.
     """
     return render_template('index.html')
+
+
 @app.route('/query-ai', methods=['POST'])
 def query_ai():
     """
@@ -22,6 +25,7 @@ def query_ai():
     response = get_ai_response(model, query)
     return jsonify(response=response)
 
+
 @app.route('/get-models', methods=['GET'])
 def get_models():
     """
@@ -31,5 +35,6 @@ def get_models():
     models = get_installed_models()
     return jsonify(models=models)
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
